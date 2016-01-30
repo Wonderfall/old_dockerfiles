a965150eea2c4147e2371a889998d7cd9429b550
Wonderfall <wonderfall@schrodinger.io>
2016-01-31 00:29:09 +0100

    add lutim

---
+++ lutim/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+deluser lutim
+addgroup --gid $GROUPID lutim
+adduser --system --shell /bin/sh --no-create-home --ingroup lutim --uid $USERID lutim
+SECRET=`cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 20 | head -n 1`
+sed -i -e 's/<contact>/'$CONTACT'/g' \
+       -e 's/<secret>/'$SECRET'/g' \
+       -e 's/<max_file_size>/'$MAX_FILE_SIZE'/g' \
+       -e 's|<webroot>|'$WEBROOT'|g' /config/lutim.conf
+mv /lutim/lutim.conf /config/lutim.conf.bkp
+cp /config/lutim.conf /lutim/lutim.conf
+chown -R lutim:lutim /lutim /data /config
+su lutim -c "carton exec hypnotoad -f /lutim/script/lutim"
